3eebb6f7697f2c89d26cc7e1c9a50260e0f83689
Liyang Ye <yeliyang1213@gmail.com>
2015-10-20 15:10:16 -0400

    Initial Commit

---
+++ proj06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj06
+//
+//  Created by Liyang Ye on 15/10/20.
+//  Copyright © 2015年 Liyang Ye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
